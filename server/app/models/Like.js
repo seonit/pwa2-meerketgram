ab24986c69e8f15a2b364d35a45c91fa21c3c5e2
seonit <seonit08@gmail.com>
2025-11-20 18:10:46 +0900

    251120 user, post, like moedel & associate ~ ing

--- server/app/models/Like.js
+++ server/app/models/Like.js
@@ -1,13 +1,13 @@
 /**
- * @file app/models/User.js
- * @description user model
+ * @file app/models/Like.js
+ * @description like model
  * 251120 v1.0.0 seon init
  */
 
 import dayjs from 'dayjs';
 import { DataTypes } from 'sequelize';
 
-const modelName = 'User' // 모델 이름 정의
+const modelName = 'Like' // 모델 이름 정의
 
 // 컬럼 정의
 const attributes = {
@@ -17,50 +17,19 @@ const attributes = {
     primaryKey: true,
     allowNull: false,
     autoIncrement: true,
+    comment: '좋아요 PK',
+  },
+  userId: {
+    field: 'user_id',
+    type: DataTypes.BIGINT.UNSIGNED,
+    allowNull: false,
     comment: '유저 PK',
   },
-  email: {
-    field: 'email',
-    type: DataTypes.STRING(100),
+  postId: {
+    field: 'post_id',
+    type: DataTypes.BIGINT.UNSIGNED,
     allowNull: false,
-    unique: true,
-    comment: '이메일(로그인ID)'
-  },
-  password: {
-    field: 'password',
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    comment: '비밀번호',
-  },
-  nick: {
-    field: 'nick',
-    type: DataTypes.STRING(15),
-    allowNull: false,
-    unique: true,
-  },
-  provider: {
-    field: 'provider',
-    type: DataTypes.STRING(10),
-    allowNull: false,
-    comment: '로그인 제공자(NONE, KAKAO, GOOGLE...)'
-  },
-  role: {
-    field: 'role',
-    type: DataTypes.STRING(10),
-    allowNull: false,
-    comment: '유저 권한(NOMAL, SUPER...)',
-  },
-  profile: {
-    field: 'profile',
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    comment: '유저 프로필',
-  },
-  refreshToken: {
-    field: 'refresh_token',
-    type: DataTypes.STRING(255),
-    allowNull: true,
-    comment: '리프래시 토큰',
+    comment: '게시글 PK'
   },
   createdAt: {
     field: 'created_at',
@@ -101,7 +70,7 @@ const attributes = {
 };
 
 const options = {
-  tableName: 'users', // 실제 DB 테이블명
+  tableName: 'likes', // 실제 DB 테이블명
   timestamps: true,  // createdAt, updatedAt를 자동 관리
   paranoid: true,   // soft delete 설정 (deletedAt 자동 관리)
 }
@@ -113,16 +82,13 @@ const User = {
     // JSON으로 serialize시, 제외할 컬럼을 지정
     define.prototype.toJSON = function() {
       const attributes = this.get();
-      delete attributes.password;
-      delete attributes.refreshToken;
-
       return attributes;
     }
 
     return define;
   },
   associate: (db) => {
-    db.User.hasMany(db.Post, { sourceKey: 'id', foreignKey: 'userId', as: 'postsUser' });
+    
   },
 }
 
