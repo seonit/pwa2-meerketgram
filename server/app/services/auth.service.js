bc31e3662149a9cca3167acc6c448072be9fe87d
seonit <seonit08@gmail.com>
2025-11-25 13:20:16 +0900

    251125 create Token  ~

--- server/app/services/auth.service.js
+++ server/app/services/auth.service.js
@@ -7,26 +7,38 @@
 import bcrypt from 'bcrypt';
 import userRepository from "../repositories/user.repository.js";
 import myError from '../errors/customs/my.error.js';
-import { NOT_REGISTERD_ERROR } from '../../configs/responseCode.config.js';
+import { NOT_REGISTERED_ERROR } from '../../configs/responseCode.config.js';
+import jwtUtil from '../utils/jwt/jwt.util.js';
 
 
 async function login(body) {
   const { email, password } = body;
 
   // email로 유저  정보 획득
-  const result = await userRepository.findByEmail(null, email);
+  const user = await userRepository.findByEmail(null, email);
 
   // 유저 존재 여부 체크
-  if(!result) {
-    throw myError('유저 미존재', NOT_REGISTERD_ERROR);
+  if(!user) {
+    throw myError('유저 미존재', NOT_REGISTERED_ERROR);
   }
 
   // 비밀번호 체크
-  if(!bcrypt.compareSync(password, result.password)) {
-    throw myError('비밀번호 틀림', NOT_REGISTERD_ERROR);
+  if(!bcrypt.compareSync(password, user.password)) {
+    throw myError('비밀번호 틀림', NOT_REGISTERED_ERROR);
   }
 
-  return result;
+  // JWT 생성(accessToken, refreshToken)
+  const accessToken = jwtUtil.generateAccessToken(user);
+  const refreshToken = jwtUtil.generateRefreshToken(user);
+
+  // refreshToken 저장
+
+
+  return {
+    accessToken,
+    refreshToken,
+    user
+  };
 }
 
 export default {
