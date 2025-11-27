1e2e7ccbc040495afa0e29976bd6123e812427e2
seonit <seonit08@gmail.com>
2025-11-27 17:23:11 +0900

    251127 multer, files uploader

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -24,6 +24,9 @@ const router = createBrowserRouter([
       {
         path: '/posts',
         element: <PostIndex />
+      },
+      {
+        
       }
     ]
   }
---
+++ server/routes/files.router.js
@@ -0,0 +1,17 @@
+/**
+ * @file router/files.router.js
+ * @description 파일 관련 라우터
+ * 251127 v1.0.0 seon init
+ */
+
+import express from 'express';
+import multerMiddleware from '../app/middlewares/multer/multer.middleware.js';
+import authMiddleware from '../app/middlewares/auth/auth.middleware.js';
+import filesController from '../app/controllers/files.controller.js';
+
+const filesRouter = express.Router();
+
+filesRouter.post('/posts', authMiddleware, multerMiddleware.postUploader, filesController.storePost);
+filesRouter.post('/profiles', authMiddleware, multerMiddleware.profileUploader, filesController.storeProfile) // 컨트롤러 입력
+
+export default filesRouter
